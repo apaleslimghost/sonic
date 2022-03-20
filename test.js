e7d6ef13bf1ff6acb4e5311aab347997a26b214a
Kara Brightwell <kara@153.io>
2022-03-20 08:08:48 +0000

    yolo

--- test.js
+++ test.js
@@ -1,18 +1,14 @@
 const { expectSingleResult, expectEOF } = require('typescript-parsec')
 const {parser, lexer} = require('./')
+const fs = require('fs')
+
+const testFile = fs.readFileSync('./test.vcl', 'utf8')
 
 console.dir(
 	expectSingleResult(
 		expectEOF(
 			parser.parse(
-				lexer.parse(`
-				if (req.http.FT-Skip-Cache == "Yes") {
-					set var.skipped = "hello";
-					if (req.http.FT-Skip-Cache == "Yes") {
-						set var.skipped = "hello";
-					}
-				}
-				`)
+				lexer.parse(testFile)
 			)
 		)
 	),
